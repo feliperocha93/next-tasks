38dd29f50bdf4c9da9f4b18dc2d197f80c6d6642
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2022-03-04 10:19:55 -0300

    Add tailwind and typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex
+      flex-col
+      justify-center
+      items-center
+      text-white
+      bg-gradient-to-tr
+      from-purple-500
+      to-yellow-600
+      h-screen
+      text-2xl
+    `}>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  )
+}
